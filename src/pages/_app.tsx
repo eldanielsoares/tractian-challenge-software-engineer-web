b3241e282ed33c3e8ecaa70e4bc9cb22cbd614f1
eldanielsoares <danielsoares7717@gmail.com>
2023-02-15 19:47:07 -0300

    add loading

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,11 +8,17 @@ import { useRouter } from "next/router";
 import { useState, useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [isLoading, setIsLoading] = useState(true);
+  const router = useRouter();
+
+  useEffect(() => {
+    router.isReady && setIsLoading(false);
+  }, [router.isReady]);
   return (
     <OverviewProvider>
       <SidebarDrawerProvider>
         <ChakraProvider theme={theme} resetCSS>
-          <Component {...pageProps} />
+          {isLoading ? <LoadingComponent /> : <Component {...pageProps} />}
         </ChakraProvider>
       </SidebarDrawerProvider>
     </OverviewProvider>
